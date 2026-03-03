29de3407441d5cc332e41d92c976f0ec7b8e5242
Samson KLOUGAN <99410105+Samriqtch@users.noreply.github.com>
2026-03-03 18:13:56 +0000

    restauration fonctions multiplier et diviser

--- src/script.js
+++ src/script.js
@@ -5,3 +5,13 @@ function addition(a, b) {
 function soustraction(a, b) {
   return a - b;
 }
+function multiplier(a, b) {
+    return a * b;
+}
+
+function diviser(a, b) {
+    if (b === 0) {
+        return "Erreur : division par zéro";
+    }
+    return a / b;
+}
